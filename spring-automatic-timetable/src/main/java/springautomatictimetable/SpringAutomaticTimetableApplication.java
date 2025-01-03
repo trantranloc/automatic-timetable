ae93c7c572df6cc7df87bc1667ee7e22ac7712cc
Loc Tran Tran <tran26122003@gmail.com>
2025-01-03 21:29:16 +0700

    Start Project

---
+++ spring-automatic-timetable/src/main/java/springautomatictimetable/SpringAutomaticTimetableApplication.java
@@ -0,0 +1,13 @@
+package springautomatictimetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutomaticTimetableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAutomaticTimetableApplication.class, args);
+    }
+
+}
---
+++ spring-automatic-timetable/src/test/java/springautomatictimetable/SpringAutomaticTimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package springautomatictimetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAutomaticTimetableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
